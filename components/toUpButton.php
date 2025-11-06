75f3d33abab6a4976cc102fb100f12b27e74714a
gramatikov88 <gramatikov88@gmail.com>
2025-11-06 12:02:44 +0200

    Бутон нагоре тест

---
+++ components/toUpButton.php
@@ -0,0 +1,4 @@
+<!-- бутон за връщане нагоре -->
+<a href="#top" class="to-up-button d-flex align-items-center justify-content-center">
+    <i class="bi bi-arrow-up-short"></i>
+</a>
--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
 require_once __DIR__ . '/config.php';
+include __DIR__ . '/components/toUpButton.php';
 $pdo = null; $homeCategories = []; $recentTests = []; $popularStr = '';
 try {
     $pdo = db();
