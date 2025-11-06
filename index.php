f6cc77f32bd9cfcbcda1ec54092bc120afecc300
gramatikov88 <gramatikov88@gmail.com>
2025-11-06 12:40:15 +0200

    Update index.php

--- index.php
+++ index.php
@@ -70,7 +70,7 @@ try {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- Bootstrap Icons -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css" rel="stylesheet">
-
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <style>
         .hero {
             background: linear-gradient(135deg, #0d6efd 0%, #6f42c1 100%);
