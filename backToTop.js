0baddd19201b295d8836a0720979024918b4af26
gramatikov88 <gramatikov88@gmail.com>
2025-11-07 11:07:36 +0200

    Update backToTop.js

--- backToTop.js
+++ backToTop.js
@@ -5,6 +5,7 @@ windwo.scroll = function () { scrollFunction() };
 function scrollFunction() {
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
         btn.style.display = "block";
+        console.log("Is working");
     } else {
         btn.style.display = "none";
     }
