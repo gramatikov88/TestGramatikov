990eb0915da3f8ca30428449626ace9c4641eff6
gramatikov88 <gramatikov88@gmail.com>
2025-11-07 11:06:50 +0200

    Update index.php

--- index.php
+++ index.php
@@ -289,6 +289,7 @@ try {
             </div>
         </div>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+        <script src="backToTop.js"></script>
     </footer>
 </body>
 
